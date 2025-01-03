0dadfdfac59ce5be7eda3e0cf2ee8b3f3d412549
kishore-1927 <kishoreanand2846@gmail.com>
2025-01-03 16:29:25 +0530

    third update

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,17 +3,17 @@ import './Skills.css'
 
 export default function Skills() {
   return (
-    <div className="container skills mt-5">
-    <div className='row'>
+    <div className=" mt-5 mb-5 container skills-btm">
+    <div className='row gap-5'>
         <h1>My Skills</h1>
-        <div className="col skill">
+        <div className="col skill-btm">
         <img width="100" height="100" src="https://img.icons8.com/fluency/100/javascript.png" alt="javascript"/>
         </div>
-        <div className="col skill">
-        <img width="100" height="100" src="https://img.icons8.com/fluency/100/javascript.png" alt="javascript"/>
+        <div className="col skill-btm">
+        <img width="100" height="100" src="https://img.icons8.com/plasticine/100/react.png" alt="react"/>
         </div>
-        <div className="col skill">
-        <img width="100" height="100" src="https://img.icons8.com/fluency/100/javascript.png" alt="javascript"/>
+        <div className="col skill-btm">
+        <img width="100" height="100" src="https://img.icons8.com/color/100/bootstrap--v1.png" alt="bootstrap--v1"/>
         </div>
     </div>
     </div>
